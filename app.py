137e2ec0c5c50fd7307d7bb72ec2c1c651d592cc
AceAltair13 <tirththoria13@hotmail.com>
2020-03-31 22:33:55 +0530

    Changed register function name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def login():
    return render_template('login.html')
 
 @app.route('/register')
-def method_name():
+def register():
    return render_template('register.html')
 
 if __name__ == '__main__':
