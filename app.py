738a8b544b8078db71d9d4ab9d7a607087dff3fc
AceAltair13 <tirththoria13@hotmail.com>
2020-05-03 20:30:32 +0530

    Password changed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'tirth'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'project'
 
 mysql = MySQL(app)
